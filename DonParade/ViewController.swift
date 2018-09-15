7d9aa406840f90913d40d00302dcb140b35289ae
pgo <waffle@tg7.so-net.ne.jp>
2018-09-15 11:18:11 +0900

    Initial Commit

---
+++ DonParade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DonParade
+//
+//  Created by takayoshi on 2018/09/15.
+//  Copyright © 2018年 pgostation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
