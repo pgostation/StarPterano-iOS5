da8c01202a0be4f8fc12b68964bcbd2c7af4d3b9
pgo <waffle@tg7.so-net.ne.jp>
2018-09-17 21:27:32 +0900

    大きくなったソースファイルを分割

---
+++ DonParade/ScrollView/TimeLineViewCell.swift
@@ -0,0 +1,115 @@
+//
+//  TimeLineViewCell.swift
+//  DonParade
+//
+//  Created by takayoshi on 2018/09/17.
+//  Copyright © 2018年 pgostation. All rights reserved.
+//
+
+import UIKit
+
+final class TimeLineViewCell: UITableViewCell {
+    static let bgColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
+    var id = ""
+    let lineLayer = CALayer()
+    let iconView = UIImageView()
+    let nameLabel = UILabel()
+    let idLabel = UILabel()
+    let dateLabel = UILabel()
+    var messageView: UIView?
+    var continueView: UILabel?
+    var boostView: UILabel?
+    weak var tableView: UITableView?
+    var indexPath: IndexPath?
+    
+    // セルの初期化
+    init(reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        
+        // デフォルトのテキストァベルは不要
+        self.textLabel?.removeFromSuperview()
+        self.detailTextLabel?.removeFromSuperview()
+        
+        // 固定プロパティは初期化時に設定
+        self.clipsToBounds = true
+        self.backgroundColor = TimeLineViewCell.bgColor
+        self.isOpaque = true
+        
+        self.iconView.layer.cornerRadius = 5
+        self.iconView.clipsToBounds = true
+        
+        self.nameLabel.textColor = UIColor(red: 0.2, green: 0.3, blue: 1.0, alpha: 1)
+        self.nameLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        self.nameLabel.backgroundColor = TimeLineViewCell.bgColor
+        self.nameLabel.isOpaque = true
+        
+        self.idLabel.font = UIFont.systemFont(ofSize: 12)
+        self.idLabel.backgroundColor = TimeLineViewCell.bgColor
+        self.idLabel.isOpaque = true
+        
+        self.dateLabel.textColor = UIColor.darkGray
+        self.dateLabel.font = UIFont.systemFont(ofSize: 12)
+        self.dateLabel.textAlignment = .right
+        self.dateLabel.backgroundColor = TimeLineViewCell.bgColor
+        self.dateLabel.isOpaque = true
+        
+        self.lineLayer.backgroundColor = UIColor.lightGray.cgColor
+        self.lineLayer.isOpaque = true
+        
+        // addする
+        self.addSubview(self.iconView)
+        self.addSubview(self.nameLabel)
+        self.addSubview(self.idLabel)
+        self.addSubview(self.dateLabel)
+        self.layer.addSublayer(self.lineLayer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // セル内のレイアウト
+    override func layoutSubviews() {
+        let screenBounds = UIScreen.main.bounds
+        
+        self.lineLayer.frame = CGRect(x: 0,
+                                      y: 0,
+                                      width: screenBounds.width,
+                                      height: 1 / UIScreen.main.scale)
+        
+        self.iconView.frame = CGRect(x: 8,
+                                     y: 10,
+                                     width: 36,
+                                     height: 36)
+        
+        self.nameLabel.frame = CGRect(x: 50,
+                                      y: 7,
+                                      width: self.nameLabel.frame.width,
+                                      height: 16)
+        
+        self.idLabel.frame = CGRect(x: 50 + self.nameLabel.frame.width + 5,
+                                    y: 7,
+                                    width: screenBounds.width - (self.nameLabel.frame.width + 50 + 5 + 50 + 5),
+                                    height: 16)
+        
+        self.dateLabel.frame = CGRect(x: screenBounds.width - 50,
+                                      y: 7,
+                                      width: 45,
+                                      height: 16)
+        
+        self.messageView?.frame = CGRect(x: 50,
+                                         y: 19,
+                                         width: self.messageView?.frame.width ?? 0,
+                                         height: self.messageView?.frame.height ?? 0)
+        
+        self.continueView?.frame = CGRect(x: screenBounds.width / 2 - 40 / 2,
+                                          y: (self.messageView?.frame.maxY ?? 0) - 6,
+                                          width: 40,
+                                          height: 18)
+        
+        self.boostView?.frame = CGRect(x: 40,
+                                       y: (self.messageView?.frame.maxY ?? 0) + 8,
+                                       width: screenBounds.width - 56,
+                                       height: 20)
+    }
+}
--- DonParade/ScrollView/TimelineView.swift
+++ DonParade/ScrollView/TimelineView.swift
@@ -16,7 +16,7 @@ final class TimeLineView: UITableView {
     private let model = TimeLineViewModel()
     private let refreshCon = UIRefreshControl()
     
-    private var accountList: [String: AccountData] = [:]
+    var accountList: [String: AnalyzeJson.AccountData] = [:]
     
     init(type: TimeLineViewController.TimeLineType, option: String?) {
         self.type = type
@@ -73,6 +73,8 @@ final class TimeLineView: UITableView {
         guard let requestUrl = url else { return }
         
         try? MastodonRequest.get(url: requestUrl) { [weak self] (data, response, error) in
+            guard let strongSelf = self else { return }
+            
             if let data = data {
                 do {
                     let responseJson = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Array<AnyObject>
@@ -82,7 +84,7 @@ final class TimeLineView: UITableView {
                     }
                     
                     if let responseJson = responseJson {
-                        self?.analyseJson(jsonList: responseJson)
+                        AnalyzeJson.analyseJson(view: strongSelf, model: strongSelf.model, jsonList: responseJson)
                     }
                 } catch {
                 }
@@ -90,165 +92,5 @@ final class TimeLineView: UITableView {
                 print(error)
             }
         }
-    }
-    
-    // タイムラインのJSONデータを解析して、リストに格納
-    private func analyseJson(jsonList: [AnyObject]) {
-        var contentList: [ContentData] = []
-        
-        var acct: String = ""
-        for json in jsonList {
-            guard let json = json as? [String: Any] else { continue }
-            
-            if let account = json["account"] as? [String: Any] {
-                acct = account["acct"] as? String ?? ""
-                let data = analyzeAccountJson(account: account)
-                self.accountList.updateValue(data, forKey: acct)
-            }
-            let reblog = json["reblog"] as? [String: Any]
-            var reblog_acct: String? = nil
-            if let account = reblog?["account"] as? [String: Any] {
-                reblog_acct = acct
-                acct = account["acct"] as? String ?? ""
-                let data = analyzeAccountJson(account: account)
-                self.accountList.updateValue(data, forKey: acct)
-            }
-            let content = json["content"] as? String
-            let created_at = json["created_at"] as? String
-            let emojis = json["emojis"] as? [[String: Any]]
-            let favourited = json["favourited"] as? Int
-            let favourites_count = json["favourites_count"] as? Int
-            let id = json["id"] as? Int64
-            let in_reply_to_account_id = json["in_reply_to_account_id"] as? String
-            let in_reply_to_id = json["in_reply_to_id"] as? Int64
-            let language = json["language"] as? String
-            let media_attachments = json["media_attachments"] as? [String]
-            let mentions = json["mentions"] as? [String]
-            let muted = json["muted"] as? Int
-            let reblogged = json["reblogged"] as? Int
-            let reblogs_count = json["reblogs_count"] as? Int
-            let replies_count = json["replies_count"] as? Int
-            let sensitive = json["sensitive"] as? Int
-            let spoiler_text = json["spoiler_text"] as? String
-            let tags = json["tags"] as? [String]
-            let uri = json["uri"] as? String
-            let url = json["url"] as? String
-            let visibility = json["visibility"] as? String
-            
-            let data = ContentData(accountId: acct,
-                                   content: content,
-                                   created_at: created_at,
-                                   emojis: emojis,
-                                   favourited: favourited,
-                                   favourites_count: favourites_count,
-                                   id: id,
-                                   in_reply_to_account_id: in_reply_to_account_id,
-                                   in_reply_to_id: in_reply_to_id,
-                                   language: language,
-                                   media_attachments: media_attachments,
-                                   mentions: mentions,
-                                   muted: muted,
-                                   reblog_acct: reblog_acct,
-                                   reblogged: reblogged,
-                                   reblogs_count: reblogs_count,
-                                   replies_count: replies_count,
-                                   sensitive: sensitive,
-                                   spoiler_text: spoiler_text,
-                                   tags: tags,
-                                   uri: uri,
-                                   url: url,
-                                   visibility: visibility)
-            contentList.append(data)
-        }
-        
-        model.change(tableView: self, addList: contentList, accountList: self.accountList)
-    }
-    
-    private func analyzeAccountJson(account: [String: Any]) -> AccountData {
-        let acct = account["acct"] as? String ?? ""
-        let avatar = account["acct"] as? String
-        let avatar_static = account["avatar_static"] as? String
-        let bot = account["bot"] as? Int
-        let created_at = account["created_at"] as? String
-        let display_name = account["display_name"] as? String
-        let emojis = account["emojis"] as? [[String: Any]]
-        let fields = account["fields"] as? [[String: Any]]
-        let followers_count = account["followers_count"] as? Int
-        let following_count = account["following_count"] as? Int
-        let header = account["header"] as? String
-        let header_static = account["header_static"] as? String
-        let id = account["id"] as? Int
-        let locked = account["locked"] as? Int
-        let note = account["note"] as? String
-        let statuses_count = account["statuses_count"] as? Int
-        let url = account["url"] as? String
-        let username = account["username"] as? String
-        
-        let data = AccountData(acct: acct,
-                               avatar: avatar,
-                               avatar_static: avatar_static,
-                               bot: bot,
-                               created_at: created_at,
-                               display_name: display_name,
-                               emojis: emojis,
-                               fields: fields,
-                               followers_count: followers_count,
-                               following_count: following_count,
-                               header: header,
-                               header_static: header_static,
-                               id: id,
-                               locked: locked,
-                               note: note,
-                               statuses_count: statuses_count,
-                               url: url,
-                               username: username)
-        return data
-    }
-    
-    struct AccountData {
-        let acct: String?
-        let avatar: String?
-        let avatar_static: String?
-        let bot: Int?
-        let created_at: String?
-        let display_name: String?
-        let emojis: [[String: Any]]?
-        let fields: [[String: Any]]?
-        let followers_count: Int?
-        let following_count: Int?
-        let header: String?
-        let header_static: String?
-        let id: Int?
-        let locked: Int?
-        let note: String?
-        let statuses_count: Int?
-        let url: String?
-        let username: String?
-    }
-    
-    struct ContentData {
-        let accountId: String
-        let content: String?
-        let created_at: String?
-        let emojis: [[String: Any]]?
-        let favourited: Int?
-        let favourites_count: Int?
-        let id: Int64?
-        let in_reply_to_account_id: String?
-        let in_reply_to_id: Int64?
-        let language: String?
-        let media_attachments: [Any]?
-        let mentions: [String]?
-        let muted: Int?
-        let reblog_acct: String?
-        let reblogged: Int?
-        let reblogs_count: Int?
-        let replies_count: Int?
-        let sensitive: Int?
-        let spoiler_text: String?
-        let tags: [String]?
-        let uri: String?
-        let url: String?
-        let visibility: String?
     }
 }
